2bbb3418cf9489d1414642a9b3c6d3dd99e0ddd5
Denis Nascimento <fdenisnascimento@gmail.com>
2018-10-30 22:13:00 -0300

    first commit

---
+++ worktime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  worktime
+//
+//  Created by Denis Nascimento on 30/10/18.
+//  Copyright © 2018 Denis Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
